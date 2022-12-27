d48544949633affd8e16c40e5b1deb7fc4cbf9e7
Gabriela Pellin Perosa <gpp260203@gmail.com>
2022-12-27 15:33:43 +0000

    Exercicio 1

---
+++ ex001.py
@@ -0,0 +1,20 @@
+n1 = int(input("Um valor: "))
+n2 = int(input("Outro valor: "))
+a = n1 + n2
+s = n1 - n2
+m = n1 * n2
+d = n1 / n2
+p = n1 ** n2
+di = n1//n2
+r = n1%n2
+rq1 = n1**(1/2)
+rq2 = n2**(1/2)
+print("A soma dos números é {}".format(n1+n2))
+print("A subtração dos números é {}".format(s))
+print("A multiplicação dos números é {}".format(m))
+print("A divisão dos números é {}".format(d))
+print("A potencia dos números é {}".format(p))
+print("A divisão inteira dos números é {}".format(di))
+print("O resto da divisão dos números é {}".format(r))
+print("A raiz quadrada do primeiro número é {}".format(rq1))
+print("A raiz quadrada do segundo número é {}".format(rq2))
